2029fda2d2a476720887b657b790e72ea13cd355
Vincent Pelletier <plr.vincent@gmail.com>
2021-09-28 14:30:26 +0000

    smartcard.app.openpgp.cli.randpin.epaper: Add battery support.

--- smartcard/app/openpgp/cli/randpin/epaper.py
+++ smartcard/app/openpgp/cli/randpin/epaper.py
@@ -17,6 +17,7 @@
 
 from collections import deque
 import errno
+import fcntl
 import functools
 import itertools
 import logging
@@ -24,11 +25,17 @@ import os
 import random
 import select
 import sys
+import threading
 import time
 import freetype
 from functionfs.gadget import (
     GadgetSubprocessManager,
     ConfigFunctionFFSSubprocess,
+)
+from gpiochip2 import (
+    EXPECT_PRECONFIGURED,
+    GPIO_V2_LINE_FLAG,
+    GPIOChip,
 )
 import ZODB
 import ZODB.FileStorage
@@ -87,7 +94,25 @@ class ICCDFunctionWithRandomPinDisplay(ICCDFunction):
     __PIN_GENERATION_DELAY = 30
     __next_pin_generation = 0
 
-    def __init__(self, path, zodb_path, display, fontface, slot_count=1):
+    __battery_capacity_path = '/sys/class/power_supply/battery/capacity'
+    __battery_x = 218
+    __battery_y = 4
+    __battery_width = 32
+    __battery_height = 11
+    __battery_thread_can_run = True
+    __battery_last_state = (None, None)
+    __battery_pipe_read = None
+    __battery_pipe_write = None
+
+    def __init__(
+        self,
+        path,
+        zodb_path,
+        display,
+        fontface,
+        slot_count=1,
+        gpiochip=None,
+    ):
         super().__init__(path=path, slot_count=slot_count)
         self.__zodb_path = zodb_path
         self.__display = display
@@ -101,6 +126,49 @@ class ICCDFunctionWithRandomPinDisplay(ICCDFunction):
         # Each item is a mapping from cell ids to contained PIN for the whole
         # generated table. Cell ids are a row name followed by a column name.
         self.__pin_queue = deque([], 2)
+        # Probe GPIO pin 4 state before opening it.
+        battery_gpio_info = gpiochip.getLineInfo(4)
+        self.__has_battery = has_battery = (
+            os.path.exists(self.__battery_capacity_path) and
+            battery_gpio_info['flags'] & GPIO_V2_LINE_FLAG.INPUT and
+            not battery_gpio_info['flags'] & GPIO_V2_LINE_FLAG.USED
+        )
+        if has_battery:
+            self.__battery_poll = battery_poll = select.poll()
+            self.__battery_thread = threading.Thread(
+                target=self.__batteryUpdateThread,
+                name='battery-monitor',
+                daemon=True,
+            )
+            self.__battery_gpio = battery_gpio = gpiochip.openLines(
+                line_list=[4],
+                flags=(
+                    GPIO_V2_LINE_FLAG.INPUT |
+                    GPIO_V2_LINE_FLAG.EDGE_RISING |
+                    GPIO_V2_LINE_FLAG.EDGE_FALLING |
+                    GPIO_V2_LINE_FLAG.BIAS_DISABLED
+                ),
+                consumer='python-smartcard'.encode('ascii'),
+                expect_preconfigured=EXPECT_PRECONFIGURED.DIRECTION,
+            )
+            fcntl.fcntl(
+                battery_gpio,
+                fcntl.F_SETFL,
+                fcntl.fcntl(battery_gpio, fcntl.F_GETFL) | os.O_NONBLOCK,
+            )
+            battery_poll.register(battery_gpio, select.POLLIN | select.POLLPRI)
+
+    def __batteryUpdateThread(self):
+        poll = self.__battery_poll.poll
+        while True:
+            # Check battery level every minute, react to events immediately
+            poll(60000)
+            # purge all available gpio events
+            self.__battery_gpio.read()
+            if not self.__battery_thread_can_run:
+                break
+            self.displayBattery()
+            self.updateDisplay(wait=False)
 
     def updateDisplay(self, wait=True):
         display = self.__display
@@ -110,6 +178,11 @@ class ICCDFunctionWithRandomPinDisplay(ICCDFunction):
             y=0,
         )
         display.swap(wait=wait)
+
+    def _blank(self, color):
+        # Battery need a redraw
+        self.__battery_last_state = (None, None)
+        self.__framebuffer.blank(color=color)
 
     def printAt(
         self,
@@ -130,19 +203,140 @@ class ICCDFunctionWithRandomPinDisplay(ICCDFunction):
         )
 
     def displayReadyUnplugged(self):
-        self.__framebuffer.blank(color=Framebuffer.COLOR_OFF)
+        self._blank(color=Framebuffer.COLOR_OFF)
         self.printAt(
             x=35,
             y=45,
             text="Ready, unplugged",
         )
+        self.displayBattery()
         self.updateDisplay(wait=False)
+
+    def displayBattery(self):
+        if not self.__has_battery:
+            return
+        with open(self.__battery_capacity_path, 'rb') as capacity:
+            capacity = int(capacity.read().decode('ascii'))
+        for level, threshold in enumerate((10, 25, 50, 75, 90, 100)):
+            if capacity < threshold:
+                break
+        charging = self.__battery_gpio.value
+        state = (level, charging)
+        if state == self.__battery_last_state:
+            return
+        self.__battery_last_state = state
+        fb = self.__framebuffer
+        battery_middle, battery_middle_remainder = divmod(self.__battery_height, 2)
+        fb.rect( # battery bump
+            self.__battery_x,
+            self.__battery_y + battery_middle - 2,
+            self.__battery_x + 2,
+            self.__battery_y + battery_middle + 2 + battery_middle_remainder,
+            color=fb.COLOR_ON,
+            fill=True,
+        )
+        fb.rect( # battery body and draw area
+            self.__battery_x + 2,
+            self.__battery_y,
+            self.__battery_x - 2 + self.__battery_width,
+            self.__battery_y + self.__battery_height,
+            color=fb.COLOR_ON,
+            fill=True,
+        )
+        if level == 0:
+            if not charging:
+                fb.printLineAt(
+                    self.__fontface,
+                    x=self.__battery_x + self.__battery_width // 2 - 2,
+                    y=self.__battery_y + 1,
+                    text="!",
+                    width=16,
+                    height=self.__battery_height,
+                    color=Framebuffer.COLOR_OFF,
+                )
+            fb.rect(
+                self.__battery_x + self.__battery_width - 5,
+                self.__battery_y + 1,
+                self.__battery_x + self.__battery_width - 3,
+                self.__battery_y - 1 + self.__battery_height,
+                color=fb.COLOR_OFF,
+                fill=True,
+            )
+        else:
+            bar_width = (self.__battery_width - 4) // 4
+            for index in range(4):
+                fb.rect(
+                    self.__battery_x + 2 + index       * bar_width + 1,
+                    self.__battery_y + 1,
+                    self.__battery_x + 2 + (index + 1) * bar_width - 1,
+                    self.__battery_y - 1 + self.__battery_height,
+                    color=fb.COLOR_OFF,
+                    fill=level > (4 - index),
+                )
+        if charging:
+            # Draw the litning bolt as two filled triangles
+            left_x = self.__battery_x + 4
+            left_y = self.__battery_y + 6
+            right_x = self.__battery_x + self.__battery_width - 4
+            right_y = self.__battery_y + 6
+            middle_x = left_x + (right_x - left_x) // 2
+            middle_top = self.__battery_y + 2
+            middle_half_width = 4
+            line = fb.line
+            for offset in range(middle_half_width + 1):
+                line(
+                    left_x, left_y,
+                    middle_x, middle_top + offset,
+                    color=Framebuffer.COLOR_ON,
+                )
+            for offset in range(middle_half_width):
+                line(
+                    middle_x, middle_top + middle_half_width + offset,
+                    right_x, right_y,
+                    color=Framebuffer.COLOR_ON,
+                )
+            # top left outline
+            line(
+                left_x, left_y,
+                middle_x, middle_top,
+                color=Framebuffer.COLOR_OFF,
+            )
+            # center left outline
+            line(
+                left_x, left_y,
+                middle_x - 1, middle_top + middle_half_width,
+                color=Framebuffer.COLOR_OFF,
+            )
+            # bottom left outline
+            line(
+                middle_x - 1, middle_top + middle_half_width,
+                middle_x, middle_top + middle_half_width + middle_half_width,
+                color=Framebuffer.COLOR_OFF,
+            )
+            # top right outline
+            line(
+                middle_x, middle_top,
+                middle_x + 1, middle_top + middle_half_width,
+                color=Framebuffer.COLOR_OFF,
+            )
+            # center right outline
+            line(
+                middle_x + 1, middle_top + middle_half_width,
+                right_x, right_y,
+                color=Framebuffer.COLOR_OFF,
+            )
+            # bottom right outline
+            line(
+                middle_x, middle_top + middle_half_width + middle_half_width,
+                right_x, right_y,
+                color=Framebuffer.COLOR_OFF,
+            )
 
     def __generatePinTable(self, tries_left, force=False):
         if not self._can_generate and not force:
             return
         fb = self.__framebuffer
-        fb.blank(color=fb.COLOR_ON)
+        self._blank(color=fb.COLOR_ON)
         # Column headers background
         fb.rect(
             0, 0,
@@ -293,6 +487,7 @@ class ICCDFunctionWithRandomPinDisplay(ICCDFunction):
                 width=self._column_width,
                 color=Framebuffer.COLOR_OFF,
             )
+        self.displayBattery()
         self.updateDisplay(wait=False)
         self.__pin_queue.append(pin_dict)
 
@@ -368,17 +563,31 @@ class ICCDFunctionWithRandomPinDisplay(ICCDFunction):
             self.__card.traverse(
                 path=(MASTER_FILE_IDENTIFIER, self.__OPENPGP_FILE_IDENTIFIER),
             ).getPIN1TriesLeft()
+        if self.__has_battery:
+            battery_pipe_read, battery_pipe_write = os.pipe()
+            self.__battery_pipe_read = battery_pipe_read = os.fdopen(battery_pipe_read, 'rb', buffering=0)
+            self.__battery_pipe_write = battery_pipe_write = os.fdopen(battery_pipe_write, 'wb', buffering=0)
+            self.__battery_poll.register(battery_pipe_read, select.POLLIN)
+            self.__battery_thread_can_run = True
+            self.__battery_thread.start()
         logger.debug('Waiting for screen to be ready...')
         self.__display.wait()
 
     def __unenter(self):
+        if self.__has_battery and self.__battery_pipe_write is not None:
+            self.__battery_thread_can_run = False
+            self.__battery_pipe_write.write(b'\x00') # XXX: is closing the write end enough ?
+            self.__battery_thread.join()
+            self.__battery_poll.unregister(self.__battery_pipe_read)
+            self.__battery_pipe_write.close()
+            self.__battery_pipe_read.close()
         if self.__connection is not None:
             self.__connection.close()
             self.__connection = None
         if self.__db is not None:
             self.__db.close()
             self.__db = None
-        self.__framebuffer.blank(color=Framebuffer.COLOR_OFF)
+        self._blank(color=Framebuffer.COLOR_OFF)
         self.printAt(
             x=85,
             y=45,
@@ -458,6 +667,7 @@ def main():
     )
     with (
         WaveShareEPaper() as display,
+        GPIOChip('/dev/gpiochip0', 'w+b') as gpiochip,
         GadgetSubprocessManager(
             args=args,
             config_list=[
@@ -473,6 +683,7 @@ def main():
                                 fontface=freetype.Face('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf'),
                                 slot_count=1,
                                 zodb_path=os.path.abspath(args.filestorage),
+                                gpiochip=gpiochip,
                             ),
                         ),
                     ],
